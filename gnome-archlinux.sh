4762e553930c8ebfebaad11143fd4e6c8f548714
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-08-15 18:33:00 -0300

    Update and rename mirrorlist to gnome-archlinux.sh

---
+++ gnome-archlinux.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo pacman -Syu gnome-calculator gnome-calendar gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menu gnome-remote-desktop gnome-session gnome-shell gnome-shell-extensions nautilus xdg-user-dirs-gtk gnome-boxes 
