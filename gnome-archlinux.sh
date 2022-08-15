25aac9fbb072ed8c980b1947ede9615580bea892
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-08-15 18:54:23 -0300

    Update gnome-archlinux.sh

--- gnome-archlinux.sh
+++ gnome-archlinux.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
-sudo pacman -Syu gnome-calculator gnome-calendar gnome-control-center gnome-disk-utility gnome-keyring gnome-logs gnome-menu gnome-remote-desktop gnome-session gnome-shell gnome-shell-extensions nautilus xdg-user-dirs-gtk gnome-boxes 
+#!/usr/bin/env bash   
+sudo pacman -Syu gnome-calculator gnome-calendar gnome-control-center gnome-terminal gnome-disk-utility gnome-keyring gnome-logs gnome-menu gnome-remote-desktop gnome-session gnome-shell gnome-shell-extensions nautilus xdg-user-dirs-gtk gnome-boxes
