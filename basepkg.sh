4205b29b5df18c8b6d1daa06f18d335c2c1c73f4
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-08-21 16:01:52 -0300

    Update basepkg.sh

--- basepkg.sh
+++ basepkg.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo pacman -Syu multilib-devel base-devel fakeroot git wget make pkg-config autoconf automake patch gvfs
+sudo pacman -Syu multilib-devel base-devel fakeroot git wget make pkg-config autoconf automake patch networkmanager gvfs
