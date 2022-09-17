ad3dd2f78c65d726314a597088e45ed4928315c2
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-09-17 19:23:32 -0300

    Update basepkg.sh

--- basepkg.sh
+++ basepkg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo pacman -Syu multilib-devel base-devel linux-firmware bash-completion fakeroot git wget make pkg-config autoconf automake patch networkmanager gvfs alsa-utils pulseaudio paprefs pavucontrol 
+sudo pacman -Syu multilib-devel base-devel mkinitcpio linux-firmware bash-completion fakeroot git wget make pkg-config autoconf automake patch networkmanager gvfs alsa-utils pulseaudio paprefs pavucontrol 
 dkms neofetch ntfs-3g file-roller unrar p7zip htop
